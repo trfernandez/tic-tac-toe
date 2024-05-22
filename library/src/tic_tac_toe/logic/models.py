e8fd345a1cc57b6199be181c1832cd565532927d
Tyler Fernandez <166658814+trfernandez@users.noreply.github.com>
2024-05-22 16:06:33 -0700

    Added game state logic
    Added game states to track that the correct order of operations occurs during the game.

--- library/src/tic_tac_toe/logic/models.py
+++ library/src/tic_tac_toe/logic/models.py
@@ -7,6 +7,9 @@ import re
 from dataclasses import dataclass
 from functools import cached_property
 
+from tic_tac_toe.logic.validators import validate_grid, validate_game_state
+from tic_tac_toe.logic.exceptions import InvalidMove
+
 WINNING_PATTERNS = (
     "???......",
     "...???...",
@@ -27,13 +30,13 @@ class Mark(str, enum.Enum):
 	def other(self) -> Mark:
 		return Mark.CROSS if self is Mark.NAUGHT else Mark.NAUGHT
 
+
 @dataclass(frozen=True)
 class Grid:
 	cells: str = " " * 9
 
 	def __post_init__(self) -> None:
-		if not re.match(r"^[\sXO]{9}$", self.cells):
-			raise ValueError("Must contain 9 cells of: X, O, or space")
+		validate_grid(self)
 
 	@cached_property
 	def x_count(self) -> int:
@@ -47,6 +50,7 @@ class Grid:
 	def empty_count(self) -> int:
 		return self.cells.count(" ")
 
+
 @dataclass(frozen=True)
 class Move:
 	mark: Mark
@@ -54,10 +58,30 @@ class Move:
 	before_state: "GameState"
 	after_state: "GameState "
 
+
 @dataclass(frozen=True)
 class GameState:
 	grid: Grid
 	starting_mark: Mark = Mark("X")
+
+	def __post_init__(self) -> None:
+		validate_game_state(self)
+
+	def make_move_to(self, index: int) -> Move:
+		if self.grid.cells[index] != " ":
+			raise InvalidMove("Cell is not empty")
+		return Move(
+			mark = self.current_mark,
+			cell_index = index,
+			before_state = self,
+			after_state = GameState(
+				Grid(
+					self.grid.cells[:index] +
+					self.current_mark +
+					self.grid.cells[index + 1:]),
+				self.starting_mark,
+				),
+			)
 
 	@cached_property
 	def current_mark(self) -> Mark:
@@ -94,4 +118,10 @@ class GameState:
 					return [match.start() for match in re.finditer(r"\?", pattern)]
 		return []
 
-
+	@cached_property
+	def possible_moves(self) -> list[Move]:
+		moves = []
+		if not self.game_over:
+			for match in re.finditer(r"\s", self.grid.cells):
+				moves.append(self.make_move_to(match.start()))
+		return moves
